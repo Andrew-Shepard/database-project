3d623af4807c9cc4eafc3175c4d6313a3d26d64e
Joshua Steege <joshuasteege@gmail.com>
2021-11-22 12:13:53 -0500

    Fixed SQL statements
    Fixed SQL statements (Not the subtypes/subclasses) and updated the data dictionary to reflect these changes.

--- sql_tables.sql
+++ sql_tables.sql
@@ -1,106 +1,109 @@
-CREATE TABLE auto.region (
-    region_id CHAR(5) GENERATED DEFAULT BY IDENTITY UNIQUE,
-    employee_id CHAR(5) NOT NULL UNIQUE,
-    region_name VARCHAR(20) NOT NULL,
-    region_zipcode CHAR(5) NOT NULL,
-    PRIMARY KEY(region_id)
-    CONSTRAINT fk_column
-        FOREIGN KEY(employee_id)
-        REFERENCES employee(employee_id)
+--CREATE TABLE
+CREATE TABLE REGION (
+    region_id        CHAR(5),
+    employee_id      CHAR(5) ,
+    region_name      VARCHAR(20),
+    region_zipcode   CHAR(5),
+    CONSTRAINT pk_region PRIMARY KEY (region_ID)
 );
-CREATE TABLE auto.store (
-    store_id CHAR(5) GENERATED DEFAULT BY IDENTITY UNIQUE,
-    employee_id CHAR(5) UNIQUE,
-    region_id CHAR(5) UNIQUE,
-    store_number_of_employees CHAR(5),
-    total_sales CHAR(5),
-    city CHAR(5),
-    PRIMARY KEY(store_id)
-    CONSTRAINT fk_column /* Unsure if it's possible to have multiple FK in oracle sql */
-        FOREIGN KEY(region_id)
-        REFERENCES region(region_id)
-        
+
+CREATE TABLE STORE (
+    store_ID                    CHAR(5),
+    employee_ID                 CHAR(5),
+    region_ID                   CHAR(5),
+    store_numSalesPersons       CHAR(5),
+    store_totalSales            CHAR(5),
+    store_city                  CHAR(5),
+    CONSTRAINT pk_store PRIMARY KEY (store_ID),
+    CONSTRAINT fk_region_ID FOREIGN KEY (region_ID) REFERENCES REGION (region_ID)
 );
-CREATE TABLE auto.employee (
-    employee_id CHAR(5) GENERATED DEFAULT BY IDENTITY UNIQUE,
-    store_id CHAR(5) ,
-    employee_email VARCHAR(20),
-    employee_salary NUMBER(9,2),
-    employee_job_title CHAR(2),
-    PRIMARY KEY(employee_id)
-    CONSTRAINT fk_column
-        FOREIGN KEY(store_id)
-        REFERENCES auto.store(store_id)
+
+CREATE TABLE EMPLOYEE (
+    employee_ID         CHAR(5),
+    store_ID            CHAR(5) ,
+    employee_email      VARCHAR(20),
+    employee_salary     NUMBER(9,2),
+    employee_jobTitle   CHAR(2),
+    CONSTRAINT pk_employee PRIMARY KEY (employee_ID),
+    CONSTRAINT fk_store_ID FOREIGN KEY (store_ID) REFERENCES STORE (store_ID)
 );
-CREATE TABLE auto.salesperson (
-    employee_id CHAR(5),
-    number_of_sales NUMBER(4,0),
-    commission NUMBER(9,2),
-    YTD_sales NUMBER(9,2),
-    PRIMARY KEY(employee_id)
-    CONSTRAINT fk_column
-        FOREIGN KEY(store_id)
-        REFERENCES auto.store(store_id)
+-- Add FK relation to region table
+alter table REGION add CONSTRAINT fk_region FOREIGN KEY 
+(employee_ID) REFERENCES EMPLOYEE (employee_ID);
+
+CREATE TABLE TRANSACTION (
+    transaction_ID         CHAR(5),
+    employee_ID            CHAR(5),
+    customer_ID            CHAR(5),
+    store_ID               CHAR(5),
+    transaction_date       DATE,
+    transaction_total      NUMBER(9,2), 
+    CONSTRAINT pk_transactions PRIMARY KEY (transaction_ID),
+    CONSTRAINT fk_employee_ID FOREIGN KEY (employee_ID) REFERENCES EMPLOYEE (employee_ID) -- Need to change to salesperson later
 );
-CREATE TABLE auto.transactions(
-    transaction_id CHAR(5),
-    employee_id CHAR(5),
-    customer_id CHAR(5),
-    store_id CHAR(5),
-    transaction_date DATE,
-    transaction_total NUMBER(9,2), 
-    PRIMARY KEY(transaction_id)
-    CONSTRAINT fk_column
-        FOREIGN KEY(employee_id)
-        REFERENCES auto.employee(employee_id)
+
+CREATE TABLE PRODUCT (
+    product_ID              CHAR(5),
+    product_isUsed          NUMBER(1),
+    product_name            VARCHAR(20),
+    product_invAmount       NUMBER(4,0),
+    product_price           NUMBER(9,2),
+    product_type            VARCHAR(20),
+    product_model           VARCHAR(20),
+    product_supplier        VARCHAR(20),
+    CONSTRAINT pk_product PRIMARY KEY (product_ID, product_isUsed)
 );
-CREATE TABLE auto.productList(
-    transaction_id CHAR(5),
-    product_id CHAR(5),
-    product_quanity NUMBER(4,0),
-    PRIMARY KEY(transaction_id, product_id)
-    CONSTRAINT fk_column
-        FOREIGN KEY(transaction_id)
-        REFERENCES auto.transactions(transaction_id)
+
+CREATE TABLE PRODUCTLIST (
+    transaction_ID      CHAR(5),
+    product_ID          CHAR(5),
+    product_isUsed      NUMBER(1),
+    product_quanity     NUMBER(4,0),
+    CONSTRAINT pk_productList PRIMARY KEY (transaction_ID, product_ID, product_isUsed),
+    CONSTRAINT fk_transaction_ID FOREIGN KEY (transaction_ID) REFERENCES TRANSACTION (transaction_ID),
+    CONSTRAINT fk_product_ID FOREIGN KEY (product_ID, product_isUsed) REFERENCES PRODUCT (product_ID, product_isUsed)
 );
-CREATE TABLE auto.products(
-    product_id CHAR(5),
-    used BOOLEAN,
-    product_name VARCHAR(20),
-    amount_in_inventory NUMBER(4,0),
-    price NUMBER(9,2),
-    product_type VARCHAR(20),
-    model_number VARCHAR(20),
-    supplier VARCHAR(20)
-    PRIMARY KEY(product_id,used)
+
+CREATE TABLE CUSTOMER (
+    customer_ID                     CHAR(5),
+    customer_name                   VARCHAR(30),
+    customer_address                VARCHAR(30),
+    customer_income                 NUMBER(9,2),
+    customer_phone                  CHAR(10),
+    customer_vehicles               NUMBER(5),
+    customer_insurance              VARCHAR(20),
+    customer_type                   CHAR(2),
+    CONSTRAINT pk_customer PRIMARY KEY (customer_ID)
 );
-CREATE TABLE auto.customers(
-    customer_id CHAR(5),
-    customer_name VARCHAR(30),
-    customer_address VARCHAR(30),
-    customer_income NUMBER(9,2),
-    customer_phone CHAR(10),
-    customer_number_of_vehicles CHAR(5),
-    customer_insurance VARCHAR(20),
-    customer_type CHAR(2)
-    PRIMARY KEY(customer_id)
+
+-- Need to figure out how to implement subclasses/subtypes
+/*
+-- Subclass of employee
+CREATE TABLE SALESPERSON (
+    employee_ID             CHAR(5),
+    sp_numSales             NUMBER(4),
+    sp_commission           NUMBER(9,2),
+    sp_YTDsales             NUMBER(9,2),
+    CONSTRAINT pk_salesperson PRIMARY KEY (employee_ID),
+    CONSTRAINT fk_employee_ID FOREIGN KEY (employee_ID) REFERENCES EMPLOYEE (employee_ID)
 );
-CREATE TABLE auto.business(
-    customer_id CHAR(5),
-    business_category VARCHAR(20),
-    business_number_of_employees CHAR(5)
-    CONSTRAINT fk_column
-        FOREIGN KEY(customer_id)
-        REFERENCES auto.customer(customer_id)
+
+-- Subclasses of customer
+CREATE TABLE BUSINESS (
+    customer_ID             CHAR(5),
+    business_category       VARCHAR(20),
+    business_numEmployees   CHAR(5),
+    CONSTRAINT pk_business PRIMARY KEY (customer_ID),
+    CONSTRAINT fk_customer_ID FOREIGN KEY (customer_ID) REFERENCES CUSTOMER (customer_ID)
 );
-CREATE TABLE auto.home(
-    customer_id CHAR(5),
-    home_marriage_status CHAR(1), /* TODO: Why is this not a boolean? */
-    home_gender CHAR(1),
-    home_age NUMBER(3,0),
-    home_number_of_residents NUMBER(3,0),
-    PRIMARY KEY(customer_id)
-    CONSTRAINT fk_column
-        FOREIGN KEY(customer_id)
-        REFERENCES auto.customer(customer_id)
+
+CREATE TABLE HOME (
+    customer_ID         CHAR(5),
+    home_marriageStat   CHAR(1), 
+    home_gender         CHAR(1),
+    home_age            NUMBER(3,0),
+    home_numResidents   NUMBER(3,0),
+    CONSTRAINT pk_home PRIMARY KEY (customer_ID),
+    CONSTRAINT fk_customer_ID FOREIGN KEY (customer_ID) REFERENCES CUSTOMER (customer_ID)
 );
+*/
