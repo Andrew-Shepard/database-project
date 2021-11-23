565dc5740c00d8d5efa5263d8b2e5cbceedbddd9
Jared Hernandez <jhernandez6615@floridapoly.edu>
2021-11-23 16:06:10 -0500

    Inserted ranges based on Data Dictionary

--- sql_tables.sql
+++ sql_tables.sql
@@ -1,6 +1,6 @@
 --CREATE TABLE
 CREATE TABLE REGION (
-    region_id        NUMBER(5) NOT NULL,
+    region_id        NUMBER(5) NOT NULL CHECK (region_ID >= 10000 AND region_ID <= 99999),
     employee_id      NUMBER(5),
     region_name      VARCHAR(20),
     region_zipcode   NUMBER(5) NOT NULL,
@@ -8,7 +8,7 @@ CREATE TABLE REGION (
 );
 
 CREATE TABLE STORE (
-    store_ID                    NUMBER(5) NOT NULL,
+    store_ID                    NUMBER(5) NOT NULL CHECK (store_ID >= 10000 AND store_ID <= 99999),
     employee_ID                 NUMBER(5),
     region_ID                   NUMBER(5),
     store_numSalesPersons       NUMBER(5),
@@ -19,12 +19,12 @@ CREATE TABLE STORE (
 );
 
 CREATE TABLE EMPLOYEE (
-    employee_ID         NUMBER(5) NOT NULL,
+    employee_ID         NUMBER(5) NOT NULL CHECK (employee_ID >= 10000 AND employee_ID <= 99999),
     store_ID            NUMBER(5),
     employee_first      VARCHAR(20),
     employee_last       VARCHAR(20),
     employee_email      VARCHAR(20),
-    employee_salary     NUMBER(9,2),
+    employee_salary     NUMBER(9,2) CHECK (employee_salary >= 10000),
     employee_jobTitle   CHAR(2),
     CONSTRAINT pk_employee PRIMARY KEY (employee_ID),
     CONSTRAINT fk_store_ID FOREIGN KEY (store_ID) REFERENCES STORE (store_ID)
@@ -32,7 +32,7 @@ CREATE TABLE EMPLOYEE (
 
 -- Subclass of employee
 CREATE TABLE SALESPERSON (
-    sp_employee_ID          NUMBER(5) NOT NULL,
+    sp_employee_ID          NUMBER(5) NOT NULL CHECK (sp_employee_ID >= 10000 AND sp_employee_ID <= 99999),
     sp_numSales             NUMBER(4),
     sp_commission           NUMBER(9,2),
     sp_YTDsales             NUMBER(9,2),
@@ -49,7 +49,7 @@ ALTER TABLE store ADD CONSTRAINT fk_employee_ID FOREIGN KEY
 (employee_ID) REFERENCES EMPLOYEE (employee_ID);
 
 CREATE TABLE TRANSACTION (
-    transaction_ID         NUMBER(5) NOT NULL,
+    transaction_ID         NUMBER(5) NOT NULL CHECK (transaction_ID >= 10000 AND transaction_ID <= 99999),
     employee_ID            NUMBER(5) NOT NULL,
     customer_ID            NUMBER(5) NOT NULL,
     store_ID               NUMBER(5) NOT NULL,
@@ -60,8 +60,8 @@ CREATE TABLE TRANSACTION (
 );
 
 CREATE TABLE PRODUCT (
-    product_ID              NUMBER(5) NOT NULL,
-    product_isUsed          NUMBER(1) NOT NULL,
+    product_ID              NUMBER(5) NOT NULL CHECK (product_ID >= 10000 AND product_ID <= 99999),
+    product_isUsed          NUMBER(1) NOT NULL CHECK (product_isUsed = 0 OR product_isUsed = 1),
     product_name            VARCHAR(20),
     product_invAmount       NUMBER(4) NOT NULL,
     product_price           NUMBER(9,2),
@@ -75,14 +75,14 @@ CREATE TABLE PRODUCTLIST (
     transaction_ID      NUMBER(5) NOT NULL,
     product_ID          NUMBER(5) NOT NULL,
     product_isUsed      NUMBER(1) NOT NULL,
-    product_quantity     NUMBER(4) NOT NULL,
+    product_quantity     NUMBER(4) NOT NULL CHECK (employee_ID >= 1 AND employee_ID <= 9999),
     CONSTRAINT pk_productList PRIMARY KEY (transaction_ID, product_ID, product_isUsed),
     CONSTRAINT fk_transaction_ID FOREIGN KEY (transaction_ID) REFERENCES TRANSACTION (transaction_ID),
     CONSTRAINT fk_product_ID FOREIGN KEY (product_ID, product_isUsed) REFERENCES PRODUCT (product_ID, product_isUsed)
 );
 
 CREATE TABLE CUSTOMER (
-    customer_ID                     NUMBER(5) NOT NULL,
+    customer_ID                     NUMBER(5) NOT NULL CHECK (customer_ID >= 10000 AND customer_ID <= 99999),
     customer_name                   VARCHAR(20) NOT NULL,
     customer_address                VARCHAR(30),
     customer_income                 NUMBER(9,2),
@@ -95,15 +95,15 @@ CREATE TABLE CUSTOMER (
 
 -- Subclasses of customer
 CREATE TABLE BUSINESS (
-    business_customer_ID    NUMBER(5) NOT NULL,
+    business_customer_ID    NUMBER(5) NOT NULL CHECK (business_customer_ID >= 10000 AND business_customer_ID <= 99999),
     business_category       VARCHAR(20) NOT NULL,
-    business_numEmployees   NUMBER(5),
+    business_numEmployees   NUMBER(5) CHECK (business_numEmployees >= 1 AND business_numEmployees <= 99999),
     CONSTRAINT pk_business PRIMARY KEY (business_customer_ID),
     CONSTRAINT fk_business_customer_ID FOREIGN KEY (business_customer_ID) REFERENCES CUSTOMER (customer_ID)
 );
 
 CREATE TABLE HOME (
-    home_customer_ID    NUMBER(5) NOT NULL,
+    home_customer_ID    NUMBER(5) NOT NULL CHECK (home_customer_ID >= 10000 AND home_customer_ID <= 99999),
     home_marriageStat   CHAR(1), 
     home_gender         CHAR(1),
     home_age            NUMBER(3),
